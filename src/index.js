199635399c2bfdd2e29901e5f57dab5350308170
Anastasiya Staravoitava <vampireslayer@mai.ru>
2019-03-14 19:44:55 +0300

    feat: ready function love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+ 
+  preferences.unshift(0);
+  let number=0;
+  for (let i=1;i<preferences.length;i++){
+    for (let k=1;k<preferences.length;k++){
+      for (let n=1;n<preferences.length;n++){
+        if(preferences[i]==i+k){
+          if(preferences[i+k]==i+n){
+            if(preferences[i+n]==i){
+              number+=1;
+            }
+          }    
+        }
+      }
+    }
+  }
+  return number 
+}
+
